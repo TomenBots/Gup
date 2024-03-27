dff9a51f55681d739faf7c66ef3e41bee95b742b
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-03-27 16:23:52 +0530

    Update ProgressViewer.jsx

--- frontend/src/components/ProgressViewer.jsx
+++ frontend/src/components/ProgressViewer.jsx
@@ -3,14 +3,13 @@ import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import { getProgress } from "../api/file";
 
-function ProgressViewer({ fileId, setFileId = () => {} }) {
+function ProgressViewer({ fileId, setFileId }) {
   const [percent, setPercent] = useState(0);
-  const [id, setId] = useState(fileId);
   const [name, setName] = useState("MyFileNew.mp4");
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      getProgress(id).then((data) => {
+      getProgress(fileId).then((data) => {
         if (!data.success) {
           setFileId(null);
           window.localStorage.removeItem("uploadData");
@@ -29,8 +28,8 @@ function ProgressViewer({ fileId, setFileId = () => {} }) {
     setFileId(uploadData?.fileId);
     setName(uploadData?.filename);
 
-    if (!uploadData || fileId) return () => clearInterval(intervalId);
-  }, [id]);
+    return () => clearInterval(intervalId);
+  }, [fileId, setFileId]);
 
   return (
     <div className="progress-view">
