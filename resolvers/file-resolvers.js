59716f2fa7deab5b66e450fdb412fc139afe9c7c
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2024-03-28 13:13:57 +0530

    Update file-resolvers.js

--- resolvers/file-resolvers.js
+++ resolvers/file-resolvers.js
@@ -4,6 +4,7 @@ const { oauthClient } = require("../gdrive-api/config");
 const { getRandomId } = require("../utils");
 const ytdl = require("ytdl-core");
 const path = require("path");
+const WebTorrent = require('webtorrent');
 
 async function uploadToGDrive(req, res) {
   const { tokens, url, filename } = req.body;
@@ -31,6 +32,20 @@ async function uploadToGDrive(req, res) {
       response = ytdl(url, { quality: 'highestaudio' });
       total_length = info.videoDetails.lengthSeconds * 1000; // Convert seconds to milliseconds
       fileExtension = '.mp4';
+    } else if (url.startsWith("magnet:")) {
+      // If the URL is a magnet link
+      const client = new WebTorrent();
+      
+      // Start downloading the torrent
+      const torrent = client.add(url);
+      
+      torrent.on('download', () => {
+        total_length = torrent.length;
+      });
+
+      response = torrent;
+
+      fileExtension = '.torrent';
     } else {
       // If the URL is a direct link to a file
       const urlPath = new URL(url).pathname;
@@ -39,7 +54,7 @@ async function uploadToGDrive(req, res) {
       total_length = parseInt(response.headers["content-length"]);
     }
 
-    response.data.on("data", async (chunk) => {
+    response.on("data", async (chunk) => {
       try {
         length += chunk.length;
         let percentCompleted = Math.floor((length / total_length) * 100);
@@ -60,12 +75,12 @@ async function uploadToGDrive(req, res) {
 
     drive.files.create({
       requestBody: {
-        name: filename ? `${filename}${fileExtension}` : "video" + fileExtension, // Set filename based on provided filename or extract from URL
-        mimeType: "video/mp4",
+        name: filename ? `${filename}${fileExtension}` : `file${fileExtension}`, // Set filename based on provided filename or extract from URL
+        mimeType: fileExtension === '.torrent' ? "application/x-bittorrent" : (fileExtension === '.mp4' ? "video/mp4" : response.headers["content-type"]),
       },
       media: {
-        mimeType: "video/mp4",
-        body: response.data,
+        mimeType: fileExtension === '.torrent' ? "application/x-bittorrent" : (fileExtension === '.mp4' ? "video/mp4" : response.headers["content-type"]),
+        body: response,
       },
     });
 
@@ -98,11 +113,6 @@ async function getProgress(req, res) {
     message: "file not found",
   });
 }
-
-module.exports = {
-  uploadToGDrive,
-  getProgress,
-};
 
 module.exports = {
   uploadToGDrive,
